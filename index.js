2fd0642582af11fcf113b9dfa23cf1c66f80fedd
devesh1808 <devesh1808@gmail.com>
2024-12-07 13:03:00 +0530

    added inquirer package code to get the hold on url

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([{ message: "Type in your URL: ", name: "URL" }])
+  .then((answers) => {
+    const url = answers.URL;
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
